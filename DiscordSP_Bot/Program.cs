a70dc15f8522f21d8251f2d61ee195dfd9abfd5f
Blueowner <messina.serafino@gmail.com>
2016-04-19 19:39:18 +0200

    First implementation of the PvE Raid command.

--- DiscordSP_Bot/Program.cs
+++ DiscordSP_Bot/Program.cs
@@ -22,6 +22,9 @@ namespace DiscordSP_Bot
 
             string m_Commandsign = "!";
 
+            string m_ResourcePath = "../../Resources/";
+            string m_FilePrefix = "pveRaid-";
+
             client.Connected += (sender, e) =>
             {
                 Console.WriteLine("Connected! User: " + e.User.Username);
@@ -50,6 +53,18 @@ namespace DiscordSP_Bot
                 {
                     e.Channel.SendMessage("http://shadowprisoners.org/");
                 }
+
+                if (e.MessageText == m_Commandsign + "pve-raid")
+                {
+                    string message = CreateRaidSquad(m_ResourcePath, m_FilePrefix);
+                    e.Channel.SendMessage(message);
+                }
+
+                if (e.MessageText == m_Commandsign + "join")
+                {
+                    string message = JoinRaidSquad(m_ResourcePath, m_FilePrefix);
+                    e.Channel.SendMessage(message);
+                }
             };
 
             try
@@ -73,5 +88,66 @@ namespace DiscordSP_Bot
             Environment.Exit(0);
 
         }
+
+        static public string CreateRaidSquad(string resourcePath, string filePrefix)
+        {
+            FileInfo[] files = GetPveRaidFiles(resourcePath, filePrefix);
+
+            string formattedDateTimeNow = DateTime.Now.ToString("yyyy-MM-d H:mm:ss tt");
+
+            if (files.Count() > 0)
+            {
+                // Pve Raid file already exists.
+                return "A raid squad has been already created. People can join with `!join`.";
+            }
+
+            StreamWriter sw = File.CreateText(
+                resourcePath + filePrefix + formattedDateTimeNow + ".txt"
+            );
+
+            sw.WriteLine(formattedDateTimeNow);
+            sw.Close();
+
+            return "The raid squad has been created! People can now join with ´!join´.";
+        }
+
+        static public string JoinRaidSquad(string resourcePath, string filePrefix)
+        {
+            FileInfo[] files = GetPveRaidFiles(resourcePath, filePrefix);
+
+            string formattedDateTimeNow = DateTime.Now.ToString("yyyy-MM-d H:mm:ss tt");
+
+            if (files.Count() == 0)
+            {
+                return "There is not raid to join.";
+            }
+
+            FileInfo file = files.First();
+
+            int lineCount = File.ReadLines(resourcePath + file.Name).Count();
+
+            if (lineCount >= 10)
+            {
+                return "Sorry, ten people have already joined the raid.";
+            }
+
+            StreamWriter sw = new StreamWriter(resourcePath + file.Name, true);
+            sw.WriteLine(formattedDateTimeNow);
+            sw.Close();
+
+            return "You have joined raid. " + (10 - lineCount) + " spot(s) left.";
+        }
+
+        static public FileInfo[] GetPveRaidFiles(string resourcePath, string filePrefix)
+        {
+            DirectoryInfo info = new DirectoryInfo(resourcePath);
+
+            return info
+                .GetFiles()
+                .Where(f => f.Name.StartsWith(filePrefix))
+                .OrderByDescending(f => f.CreationTime)
+                .ToArray();
+        }
+
     }
 }
